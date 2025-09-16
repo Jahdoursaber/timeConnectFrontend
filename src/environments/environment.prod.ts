1abb647d0eed25f51c5d249e939c2bc96048534c
Jahdoursaber <saber_jahdour@outlook.com>
2025-09-16 13:59:42 +0100

    corriger url de adresse public

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://13.36.238.251:8000/',
-  baseUrl: 'http://13.36.238.251:8000/api/spa/'
+  apiUrl: 'http://13.37.201.64:8000/',
+  baseUrl: 'http://13.37.201.64:8000/api/spa/'
 };
